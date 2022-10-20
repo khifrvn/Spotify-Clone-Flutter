0c51be0b787cacb8d4d211f7cf1074b77943bf27
Khifransyah <khifrvn@gmail.com>
2022-10-20 19:08:34 +0700

    Update home.dart

--- latihan1/lib/home.dart
+++ latihan1/lib/home.dart
@@ -627,6 +627,9 @@ class _HomePageState extends State<HomePage> {
                         )
                       ],
                     ),
+                    SizedBox(
+                      height: 20.h,
+                    )
                   ],
                 ),
               )
